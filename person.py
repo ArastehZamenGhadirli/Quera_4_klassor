2aeed7abec5d7ff3499e051e1661369e25f45f5c
Arasteh Zamen Ghadirli <41484641+ArastehZz@users.noreply.github.com>
2025-02-16 19:26:47 +0330

    finished

--- person.py
+++ person.py
@@ -1,6 +1,25 @@
-class Person :
-    def __init__(self,name,age):
-        self.name=name
-        self.age=age
-    def birthday(self,age):
-        
+class Person:
+    """
+    A class to represent a person with a name and age.
+    """
+
+    def __init__(self, name: str, age: int):
+        """
+        Initialize a Person object.
+        Parameters:
+        name (str): The name of the person.
+        age (int): The age of the person.
+        """
+        self.name = name
+        self.age = age
+
+    def birthday(self):
+        """
+        Increases the person's age by 1.
+        """
+        self.age += 1
+
+
+p = Person("Alice", 25)
+p.birthday()
+print(p.age)
